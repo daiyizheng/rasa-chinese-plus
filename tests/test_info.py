f404b0308c6f67248943c3ee7964e50ffed86980
daiyizheng <387942239@qq.com>
2021-11-19 14:08:34 +0800

    init

---
+++ tests/test_info.py
@@ -0,0 +1,15 @@
+# -*- coding:UTF-8 -*-
+
+# author:user
+# contact: test@test.com
+# datetime:2021/11/19 13:29
+# software: PyCharm
+
+"""
+文件说明：
+    
+"""
+
+from rasa_chinese_plus import info
+
+print(info.info("xxx"))
